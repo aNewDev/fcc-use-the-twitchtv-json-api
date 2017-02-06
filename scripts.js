76880e06ca7137a68751991df58966c7b983f4d2
aNewDev <iam@anewdev.io>
2017-02-06 13:39:18 -0600

    Formatting improvements
    Closes #5 - Added a header called Twitch Streams
    Closes #6 - Added Google fonts
    Closes #7 - Added background, header, and row colors

--- scripts.js
+++ scripts.js
@@ -3,42 +3,39 @@ $(document).ready(function() {
   getDataForAllChannels();
 });
 
-var channels = ["esl_sc2", "comster404", "freecodecamp", "Brawlhalla"];  //
+var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "comster404", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];  //
 
 function getDataForAllChannels() {
   channels.forEach(function(channel) {
     $.getJSON("https://wind-bow.gomix.me/twitch-api/channels/" + channel + "?callback=?", function(data) {
-      console.log(data);
       var status, url, name, logo;
       if (data.error === "Not Found") {
         status = false;
-        url = "https://twitch.tv";
-        name = channel;
+        url = '';
+        name = '<div class="col-md-2">' + channel + '</div>';
         logo = "https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F";
       } else {
         status = true;
-        url = data.url;
-        name = data.name;
+        url = '<a href="' + data.url + '" target="_blank">';
+        name = '<div class="col-md-2">' + data.name + '</div></a>';
         logo = data.logo;
       };
       $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/" + channel + "?callback=?", function(streamdata) {
-        console.log(streamdata);
         if (status === false) {
-          status = "Not a known twitch channel";
+          status = "Not on twitch";
         } else if (streamdata.stream === null) {
-          status = "Not currently broadcasting";
+          status = "Offline";
         } else {
-          console.log(streamdata.stream.channel);
-          url = streamdata.stream.channel.url;
-          status = '<a href="' + url + '">' + streamdata.stream.channel.status + '</a>';
+          url = '<a href="' + streamdata.stream.channel.url + '" target="_blank">';
+          status = '<a href="' + streamdata.stream.channel.url + '">' + streamdata.stream.channel.status + '</a>';
 
         }
-        var html = '<div class="row text-center"><div class="col-md-3"></div><a href="' +
-          url + '" target="_blank"><div class="col-md-1"><img src="' +
-          logo + '"></div><div class="col-md-2">' +
-          name + '</div></a><div class="col-md-3">' +
-          status + '</div><div class="col-md-3"></div></div>';
-        document.getElementById("chicken").innerHTML += html;
+        var html = '<div class="row text-center" id="datarow">' +
+          url + '<div class="col-xs-2 col-sm-1"><img src="' +
+          logo + '"></div><div class="col-xs-10 col-sm-3">' +
+          name + '</div><div class="col-xs-10 col-sm-8"">' +
+          status + '</div></div>';
+        document.getElementById("rows").innerHTML += html;
       });
     });
   });
