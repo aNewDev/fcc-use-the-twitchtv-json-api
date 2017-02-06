271e4da613d1adc8d526e5a562dd698d4ccfd218
aNewDev <iam@anewdev.io>
2017-02-06 14:07:21 -0600

    Final
    Closes #8 - decided to not add this functionality
    Closes #9 - Added commenting to the document

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
-//
+// trigger data load function on page load
 $(document).ready(function() {
   getDataForAllChannels();
 });
 
-var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "comster404", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];  //
+// Set the channels to use in data load
+var channels = ["ESL_SC2", "OgamingSC2", "brunofin", "cretetion", "comster404", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];  //
 
+// Call for data from Twitch API
 function getDataForAllChannels() {
   channels.forEach(function(channel) {
+    // Data from the Channels API
     $.getJSON("https://wind-bow.gomix.me/twitch-api/channels/" + channel + "?callback=?", function(data) {
       var status, url, name, logo;
       if (data.error === "Not Found") {
@@ -20,6 +23,7 @@ function getDataForAllChannels() {
         name = '<div class="col-md-2">' + data.name + '</div></a>';
         logo = data.logo;
       };
+      // Data from the Streams API
       $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/" + channel + "?callback=?", function(streamdata) {
         if (status === false) {
           status = "Not on twitch";
@@ -28,8 +32,8 @@ function getDataForAllChannels() {
         } else {
           url = '<a href="' + streamdata.stream.channel.url + '" target="_blank">';
           status = '<a href="' + streamdata.stream.channel.url + '">' + streamdata.stream.channel.status + '</a>';
-
         }
+        // Build the page from the pulled data
         var html = '<div class="row text-center" id="datarow">' +
           url + '<div class="col-xs-2 col-sm-1"><img src="' +
           logo + '"></div><div class="col-xs-10 col-sm-3">' +
@@ -40,7 +44,3 @@ function getDataForAllChannels() {
     });
   });
 };
-
-/*
-
-*/
